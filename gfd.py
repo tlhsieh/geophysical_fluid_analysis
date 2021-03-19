e8d9450a8b3be53369239458eee6a404e402d928
Tsung-Lin Hsieh <hsiehtl@princeton.edu>
2021-03-19 12:26:46 -0400

    add nearest_mean

--- gfd.py
+++ gfd.py
@@ -6,6 +6,41 @@ def get_fcor(lat):
 def get_beta(lat):
     return 2*2*np.pi/86164/6371e3*np.cos(lat/180*np.pi)
     
+def nearest_mean(x_in, y_in, z_in, x_out, y_out, n_nearest=6, decay=4):
+    """Given z(x, y), find z(x', y'), in which x' and y' are not on the x, y grid
+    
+    Example:
+        slp_track = nearest_mean(slp.lon, slp.lat, slp.values, lon_track, lat_track)
+
+    Args:
+        x_in (1d or 2d array)
+        y_in (1d or 2d array)
+        z_in (2d array)
+        x_out (1d array)
+        y_out (1d array)
+        n_nearest (int): number of nearest input grid points to average over; the smaller the faster
+        decay (int): exponent over 1/dist^2; the larger the more local
+
+    Returns:
+        z_out (1d array)
+    """
+    
+    assert len(x_out) == len(y_out)
+    
+    small = 1e-9
+    
+    xx, yy = np.meshgrid(x_in, y_in)
+    weights = [1/((xx - x_out[i])**2 + (yy - y_out[i])**2 + small)
+               for i in range(len(x_out))] # weights proportional to 1/dist^2
+    
+    idx_large = [np.argsort(weight.flatten())[-n_nearest:] for weight in weights] # indices of the largest weights
+    weights_large = [weights[i].flatten()[idx_large[i]] for i in range(len(idx_large))]
+    
+    z_out = [np.average(z_in.flatten()[idx_large[i]], weights=weights_large[i]**decay)
+             for i in range(len(idx_large))]
+    
+    return np.array(z_out)
+
 def geoaxes(axes):
     '''
     Axes settings for geographical maps
